1804a05ce566f518d2adc4d143a563931345aba7
Anshul Kayastha <anshul.kayastha@emc.com>
2016-07-28 16:22:59 +0530

    Add DELETE todos route

--- server.js
+++ server.js
@@ -31,17 +31,26 @@ app.get('/todos/:id', function(req, res) {
 
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
-
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         res.status(400).send();
     }
-
     body.description = body.description.trim();
     body.id = nextTodoId;
     nextTodoId++;
     todos.push(body);
     res.json(body);
 });
+
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if(!matchedTodo) {
+        res.status(404).json({"error": "no todo found with that id"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+})
 
 app.listen(PORT, function() {
     console.log('Express Server started on Port '+PORT);
