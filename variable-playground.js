c824fb9efde82085728ed4d88de25db4e8688118
Anshul Kayastha <anshul.kayastha@emc.com>
2016-08-01 23:13:17 +0530

    Add variable-playground

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+//var person = {
+//    name : 'Anshul',
+//    age: 30
+//}
+//
+//function updatePerson(obj) {
+//    //obj = {
+//    //    name: 'Anshul',
+//    //    age: 36
+//    //}
+//    obj.age = 40;
+//}
+//updatePerson(person);
+//console.log(person);
+
+var grades = [5, 15];
+
+function updateGrades(obj) {
+    obj.push(25);
+   // obj = [5, 20, 50];
+}
+
+updateGrades(grades);
+console.log(grades);
